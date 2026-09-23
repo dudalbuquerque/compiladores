cc547e01b973fa04bc3d431964ce237813edafed
julia-zovka <jzs@cin.ufpe.br>
2026-09-23 00:22:30 -0300

    add  error tests, possible new running mode

--- mini_sql/tests/parser_error_test.rs
+++ mini_sql/tests/parser_error_test.rs
@@ -10,16 +10,50 @@ fn parse_error(input: &str) -> bool {
     let mut parser = MiniSqlParser::new(tokens);
 
     parser.program().is_err()
+    // Executa o parser
+    //let result = parser.program();
+
+    // Se houver algum erro de sintaxe registrado pelo parser ou se a função retornar Err
+    //result.is_err() || parser.get_number_of_syntax_errors() > 0
 }
 
+
+
+// users
+// name age type credit active
+
+
+
+
+/// syntax error tests
 #[test]
 fn test_missing_select_list() {
     assert!(parse_error("FROM users SELECT;"));
 }
 
 #[test]
+fn test_missing_select_list_minus() {
+    assert!(parse_error("from users select;"));
+}
+
+
+#[test]
+fn test_missing_end() {
+    assert!(parse_error("FROM users SELECT name"));
+}
+
+#[test]
+fn test_missing_end_minus() {
+    assert!(parse_error("from users select name"));
+}
+
+#[test]
 fn test_missing_select() {
     assert!(parse_error("FROM users name;"));
+}
+#[test]
+fn test_missing_select_minus() {
+    assert!(parse_error("from users name;"));
 }
 
 #[test]
@@ -28,10 +62,65 @@ fn test_missing_from() {
 }
 
 #[test]
+fn test_missing_from_minus() {
+    assert!(parse_error("select name;"));
+}
+
+#[test]
 fn test_missing_where_condition() {
     assert!(parse_error("FROM users SELECT name WHERE;"));
 }
 
+#[test]
+fn test_missing_where_condition_minus() {
+    assert!(parse_error("from users select name where;"));
+}
+
+
+#[test]
+fn test_trailing_comma_in_select() {
+    assert!(parse_error("FROM users SELECT name, age, WHERE active = FALSE;"));
+}
+
+
+#[test]
+fn test_trailing_comma_in_select_minus() {
+    assert!(parse_error("from users select name, age, where active = false;"));
+}
+
+
+#[test]
+fn test_empty_in_clause() {
+    assert!(parse_error("FROM users SELECT name WHERE age IN ();"));
+}
+
+#[test]
+fn test_empty_in_clause_minus() {
+    assert!(parse_error("from users select name where age in ();"));
+}
+
+
+#[test]
+fn test_unclosed_string_literal() {
+    assert!(parse_error("FROM users SELECT name WHERE type = 'admin;"));
+}
+#[test]
+fn test_unclosed_string_literal_minus() {
+    assert!(parse_error("from users select name where type = 'admin;"));
+}
+
+#[test]
+fn test_invalid_operator() {
+    // Operadores como === ou && não foram definidos na gramática
+    assert!(parse_error("FROM users SELECT name WHERE age === 18;"));
+}
+#[test]
+fn test_invalid_operator_minus() {
+    // Operadores como === ou && não foram definidos na gramática
+    assert!(parse_error("from users select name where age === 18;"));
+}
+
+////////// typo tests
 #[test]
 fn test_typo_in_from() {
     assert!(parse_error(
@@ -40,9 +129,9 @@ fn test_typo_in_from() {
 }
 
 #[test]
-fn test_invalid_table_syntax() {
+fn test_typo_in_from_minus() {
     assert!(parse_error(
-        "FROM users()_uugf SELECT name WHERE age > 18 OR active = TRUE;"
+        "form users select name where age > 18 and active = true;"
     ));
 }
 
@@ -52,8 +141,35 @@ fn test_typo_in_select() {
 }
 
 #[test]
+fn test_typo_in_select_minus() {
+    assert!(parse_error("from users selec *;"));
+}
+
+
+////////// identifier tests
+
+#[test]
+fn test_invalid_table_syntax() {
+    assert!(parse_error(
+        "FROM users()_uugf SELECT name WHERE age > 18 OR active = TRUE;"
+    ));
+}
+
+#[test]
+fn test_invalid_table_syntax_minus() {
+    assert!(parse_error(
+        "from users()_uugf select name where age > 18 or active = true;"
+    ));
+}
+
+#[test]
 fn test_identifier_start_with_number() {
     assert!(parse_error("FROM 123users SELECT name;"));
+}
+
+#[test]
+fn test_identifier_start_with_number_minus() {
+    assert!(parse_error("from 123users select name;"));
 }
 
 #[test]
@@ -62,6 +178,30 @@ fn test_invalid_identifier_start() {
 }
 
 #[test]
+fn test_invalid_identifier_start_minus() {
+    assert!(parse_error("from _users select name;"));
+}
+
+#[test]
+fn test_uppercase_identifier() {
+    // O lexer só aceita identificadores começando com minúscula: [a-z] [a-z0-9_]*
+    assert!(parse_error("FROM Users SELECT name;"));
+}
+
+#[test]
+fn test_uppercase_identifier_minus() {
+    // O lexer só aceita identificadores começando com minúscula: [a-z] [a-z0-9_]*
+    assert!(parse_error("from Users select name;"));
+}
+
+
+////////// old syntax tests
+#[test]
 fn test_old_select_syntax() {
     assert!(parse_error("SELECT name FROM users;"));
+}
+
+#[test]
+fn test_old_select_syntax_minus() {
+    assert!(parse_error("select name from users;"));
 }
